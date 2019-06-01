c3f8c7731acf2f18d826e0ed4dd8b8d6d72b738a
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-06-01 20:55:52 -0300

    update readme gh-page

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lit-unlit-react-game
+
 Lit Unlit React game
+
+# Play on
+
+https://elpoeta.github.io/lit-unlit-react-game/
---
+++ build/precache-manifest.d10f4a12821c6310b5519678ec6a67f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1cb6c36f34b4ff58fad93c8612a400d",
+    "url": "/lit-unlit-react-game/index.html"
+  },
+  {
+    "revision": "86252c10f34e7ba9d5c8",
+    "url": "/lit-unlit-react-game/static/css/main.00b4ed1a.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/lit-unlit-react-game/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "86252c10f34e7ba9d5c8",
+    "url": "/lit-unlit-react-game/static/js/main.078b9e26.chunk.js"
+  },
+  {
+    "revision": "6ba510802e80ac7def88",
+    "url": "/lit-unlit-react-game/static/js/runtime~main.adbc1644.js"
+  }
+]);
