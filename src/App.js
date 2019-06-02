87caa63a46d31a275c4340503f61ef078cde1bee
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-06-02 18:14:14 -0300

    add float button change title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Board from "./components/Board";
+import FloatButton from "./components/FloatButton";
 const App = () => {
   return (
     <div className="App">
       <Board rows={5} cols={5} />
+      <FloatButton />
     </div>
   );
 };
---
+++ src/components/FloatButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./FloatButton.css";
+
+const FloatButton = () => {
+  return (
+    <div>
+      <div className="FloatButton" />
+    </div>
+  );
+};
+
+export default FloatButton;
